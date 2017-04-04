be849b4ab38533c0e2d789a6a7cd7d5da01df492
Ashish patel <ashishpatel0720@gmail.com>
2017-04-05 03:59:03 +0530

    creating line chart

--- public/js/result.js
+++ public/js/result.js
@@ -1,1 +1,98 @@
 
+$('document').ready(function () {
+    makeTables();
+});
+// $(document).ajaxSuccess(function(){
+//     makeLineChart();
+//     makeBarChart();
+// });
+
+function makeTables() {
+    var training_label=[];
+    var training_val=[];
+
+    var predicted_val=[];
+    var predicted_label=[];
+    var counter = 1975;
+    var training_file = $("#tdata").text();
+    var predicted_file = $("#pdata").text();
+    var training_label=[];
+    var training_val=[];
+
+    var predicted_val=[];
+    var predicted_label=[];
+
+
+    //getting the data from training file
+    $.getJSON(training_file, function (data) {
+        data = data.yeild;
+        $.each(data, function (key, val) {
+            $("#previous-data").append("<tr><td>" + counter + "</td>" + "<td>" + val + "</td></tr>");
+            training_label.push(counter);
+            training_val.push(val);
+            counter++;
+        });
+    });
+
+    //getting data from predicted file
+    $.getJSON(predicted_file, function (data) {
+        data = data.yeild; // so that we can get real data
+        $.each(data, function (key, val) {
+            predicted_label.push(counter);
+            predicted_val.push(val);
+            $("#predicted-data").append("<tr><td>" + counter + "</td>" + "<td>" + val + "</td></tr>");
+            counter++;
+        });
+    });
+    $(function(){
+        $("tbody#previous-data").each(function(elem,index){
+            var arr = $.makeArray($("tr",this).detach());
+            arr.reverse();
+            $(this).append(arr);
+        });
+    });
+}
+
+function makeLineChart() {
+
+    var cnv=$("#line-chart");
+
+    //TODO: may be the problem is because of the fact that ajax loads after we have done these things.
+
+    //TODO: create arrays of lables and data
+    var label_arr=[];
+        $("#previous-data tr td:odd").each(function(key,value){
+       label_arr.push($(value).html());
+    });
+    var value_arr=[];
+    $("#previous-data tr td:even").each(function(key,value){
+        value_arr.push($(value).html());
+    });
+    console.log("something happening");
+    console.log(label_arr);
+    console.log(value_arr);
+
+    //TODO: make data for charting using arrays
+    // var ap-data={
+    //     data-set:[{
+    //
+    //    }],
+    // };
+
+    //TODO: make chart
+    // var chart=new Chart(cnv,{
+    //     label:'Predictions',
+    //     type:'line',
+    //    data:ap-data
+    // });
+
+
+
+}
+function makeBarChart(){
+//TODO: make bar chart with same way as line chart
+
+
+}
+
+
--- resources/views/result.blade.php
+++ resources/views/result.blade.php
@@ -1,72 +1,68 @@
 @extends("layouts.master")
-@section('content')
-<script src="/js/form.js"></script>
-<p id="tdata" hidden>data/{{$training_data}}</p>
-<p id="pdata" hidden>data/{{$predicted_data}}</p>
 
-<div class="container">
+@section('content')
+    {{--for getting the name of file dynamically--}}
+    <span id="tdata" hidden>data/{{$training_data}}</span>
+    <span id="pdata" hidden>data/{{$predicted_data}}</span>
+
+    <div class="container">
         <div class="row">
-            <div class="col-sx-8  col-md-4 table-responsive">
-                <table class="table table-hover  table-bordered" id="ap-table">
+            <div class="col-md-6 col-sx-12">
+                <canvas  id="line-chart"  width="100%" height="100%">
+
+                </canvas>
+
+
+            </div>
+            <div class="col-md-6 col-sx-12">
+                <canvas id="bar-chart" width="100%" height="100%">
+
+                </canvas>
+            </div>
+
+
+        </div>
+
+
+    </div>
+
+
+
+
+
+    <div class="container">
+        <div class="row">
+            <div class="col-sx-6  col-md-4 table-responsive">
+                <table   class="table table-hover  table-bordered">
                     <thead>
                     <tr>
                         <th>Year</th>
-                        <th>Prediction</th>
+                        <th>Yeild (Hg per Hactare)</th>
                     </tr>
                     </thead>
-                    <tbody id="ap-row">
+                    <tbody id="previous-data">
                     <tr >
                     </tr>
                     </tbody>
                 </table>
             </div>
-
+            <div class="col-sx-6  col-md-4">
+                <table  class=" table-responsive table table-hover  table-bordered" id="ap-table">
+                    <thead>
+                    <tr>
+                        <th>Year</th>
+                        <th>Predicted Yield (Hg per Hactare)</th>
+                    </tr>
+                    </thead>
+                    <tbody id='predicted-data'>
+                    <tr >
+                    </tr>
+                    </tbody>
+                </table>
+            </div>
         </div>
     </div>
+    <script src="/js/chart.min.js"></script>
+    <script src="/js/result.js"></script>
 
-    {{--<script type="text/javascript">--}}
-
-        {{--var training_label=[];--}}
-        {{--var training_val=[];--}}
-        {{--var predicted_val=[];--}}
-        {{--var predicted_label=[];--}}
-        {{--var tlength,plength;--}}
-
-        {{--$.getJSON("data/{{$training_data}}",function(data) {--}}
-            {{--data=data.yeild; // so that we can get real data--}}
-            {{--$.each(data,function(key,val) {--}}
-{{--//                console.log("while appending, length",training_val.length);--}}
-                {{--if(training_label.length==0)--}}
-                    {{--training_label.push(1975);--}}
-                {{--else--}}
-                    {{--training_label.push(training_label[training_label.length-1]+1);--}}
-                {{--tlength =training_val.push(val);--}}
-            {{--});--}}
-        {{--});--}}
-        {{--$.getJSON("data/{{$predicted_data}}",function(data) {--}}
-            {{--data=data.yeild;--}}
-            {{--$.each(data,function(key,val) {--}}
-{{--//                console.log("while appending, length",predicted_val.length);--}}
-                {{--if(predicted_label.length==0)--}}
-                    {{--predicted_label.push(training_label[training_label.length-1]+1);--}}
-                {{--else--}}
-                    {{--predicted_label.push(predicted_label[predicted_label.length - 1] + 1);--}}
-                {{--plength=predicted_val.push(val);--}}
-            {{--});--}}
-        {{--});--}}
-        {{--console.log(training_label);--}}
-        {{--alert(training_label.length);--}}
-        {{--console.log(training_val);--}}
-        {{--console.log(predicted_label);--}}
-        {{--console.log(predicted_val);--}}
-        {{--console.log("length is  "+training_val.length);--}}
-        {{--console.log("length is  "+predicted_val.length);--}}
-        {{--console.log("length is  "+training_label.length);--}}
-        {{--console.log("length is  "+predicted_label.length);--}}
-
-        {{--for(var i=0;i<training_val.length;i++){--}}
-            {{--console.log("getting data");--}}
-            {{--$("#ap-row").append("<tr><td>"+training_label[i]+"</td>"+"<td>"+training_val[i].toFixed(0)+"</td></tr>");--}}
-        {{--}--}}
-    {{--</script>--}}
 @endsection
